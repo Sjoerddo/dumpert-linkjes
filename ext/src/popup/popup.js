172ea69d345845865707c3f023578d0d6f4dcb19
RussianDaddy <sjoerd.weerstra@hotmail.com>
2017-05-01 23:16:51 +0200

    Style changes

--- ext/src/popup/popup.js
+++ ext/src/popup/popup.js
@@ -2,8 +2,11 @@ chrome.runtime.sendMessage({type: "GetComments"});
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     var comments = document.createElement('section');
+    var html = message.html;
 
-    comments.innerHTML = message.html;
+    comments.innerHTML = html
+        ? html
+        : 'Geen linkjes gevonden';
 
     document.getElementById('content').appendChild(comments);
 });
