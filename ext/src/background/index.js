8d8f448052a5f6b41a6292fc4a2d4bd82e591228
RussianDaddy <sjoerd.weerstra@hotmail.com>
2017-05-11 21:23:02 +0200

    Sorteer op kudos

--- ext/src/background/index.js
+++ ext/src/background/index.js
@@ -8,20 +8,23 @@ var NO_LINKS_FOUND = 'Geen linkjes gevonden';
 
 chrome.extension.onMessage.addListener(function (request) {
     if (request.type === 'GetComments') {
-        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             var tabUrl = tabs[0].url;
             var key = getSessionKey(tabUrl);
 
             var stored = sessionStorage.getItem(key);
             if (stored) {
-                chrome.runtime.sendMessage({html: stored});
+                chrome.runtime.sendMessage({ html: stored });
             } else {
                 var apiUrl = 'https://i321720.iris.fhict.nl/php/linkjes_scrape.php?target=' + tabUrl;
 
                 fetchHtml(apiUrl).then(function (html) {
-                    html = html || NO_LINKS_FOUND;
+                    html = html
+                        ? sortHtmlByKudos(html)
+                        : NO_LINKS_FOUND;
+                    
                     sessionStorage.setItem(key, html);
-                    chrome.runtime.sendMessage({html: html});
+                    chrome.runtime.sendMessage({ html: html });
                 });
             }
         });
@@ -31,6 +34,17 @@ chrome.extension.onMessage.addListener(function (request) {
 function fetchHtml(url) {
     return fetch(url).then(function (response) {
         return response.text();
+    });
+}
+
+function sortHtmlByKudos(html) {
+    var section = document.createElement('section');
+    section.innerHTML = html;
+
+    return Array.from(section.querySelectorAll('article')).sort(function (a, b) {
+        return b.getAttribute('data-kudos') - a.getAttribute('data-kudos');
+    }).map(function (article) {
+        return article.outerHTML;
     });
 }
 
